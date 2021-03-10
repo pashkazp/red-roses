6a5bd9964b00384cdfaee2ab45b0d7df40ed556f
DePavlo <pashka@ukr.net>
2021-03-10 16:27:42 +0200

    Initial Init

---
+++ million-red-roses/src/main/java/depavlo/millionredroses/MillionRedRosesApplication.java
@@ -0,0 +1,13 @@
+package depavlo.millionredroses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillionRedRosesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MillionRedRosesApplication.class, args);
+	}
+
+}
---
+++ million-red-roses/src/test/java/depavlo/millionredroses/MillionRedRosesApplicationTests.java
@@ -0,0 +1,13 @@
+package depavlo.millionredroses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillionRedRosesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
