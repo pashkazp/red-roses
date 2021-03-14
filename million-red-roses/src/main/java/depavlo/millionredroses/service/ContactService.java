0aec303468c911fdb91b7635f090e952fa2b4a67
DePavlo <pashka@ukr.net>
2021-03-14 22:10:56 +0200

    polish

--- million-red-roses/src/main/java/depavlo/millionredroses/service/ContactService.java
+++ million-red-roses/src/main/java/depavlo/millionredroses/service/ContactService.java
@@ -1,6 +1,5 @@
 package depavlo.millionredroses.service;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Iterator;
@@ -14,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import depavlo.millionredroses.model.Contact;
@@ -36,37 +36,44 @@ public class ContactService implements StreamingResponseBody {
 	@Override
 	@Transactional(readOnly = true)
 	public void writeTo(OutputStream outputStream) {
+
+		log.debug("writeTo] - Write filtered contacts to OutputStream {}", outputStream);
+
 		Pattern pattern = Pattern.compile(filter);
+
 		try (Stream<Contact> contactResultStream = dao.getAll()) {
 			try (PrintWriter oos = new PrintWriter(outputStream)) {
+
 				Iterator<Contact> ci = contactResultStream.iterator();
 
-				oos.write("[\n");
+				oos.write("[\n"); // begin Contacts array
 
-				if (ci.hasNext()) {
+				if (ci.hasNext()) {// if there is at least one contact
 
-					while (true) {
-						Contact contact = ci.next();
-						Matcher matcher = pattern.matcher(contact.getName());
-						if (!matcher.matches()) {
-							oos.write(new ObjectMapper().writeValueAsString(contact));
-							if (ci.hasNext()) {
-								oos.write(",\n");
-								oos.flush();
+					while (true) { // endless loop
+						Contact contact = ci.next(); // get contact
+						Matcher matcher = pattern.matcher(contact.getName()); // test name field by filter
+						if (!matcher.matches()) { // in Not match
+
+							entityManager.detach(contact); // clean persistent context
+
+							oos.write(new ObjectMapper().writeValueAsString(contact)); // write JSON representation of
+																						// Contact
+							if (ci.hasNext()) { // if there is one more contact
+								oos.write(",\n"); // delimit JSON records
+								oos.flush(); // flush output
 							} else {
-								break;
+								break; // if there are no more contacts, interrupt the endless loop
 							}
 						}
-						entityManager.detach(contact);
 					}
 				}
 
-				oos.write("\n]");
-				oos.flush();
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				oos.write("\n]"); // end Contacts array
+				oos.flush(); // flush output
+			} catch (JsonProcessingException e) {
+				log.error("writeTo] - Exception {}", e);
+				throw new RuntimeException("Error on JSON processing. " + e.getMessage(), e.getCause());
 			}
 		}
 	}
--- million-red-roses/src/main/java/depavlo/millionredroses/ui/rest/v1/ContactController.java
+++ million-red-roses/src/main/java/depavlo/millionredroses/ui/rest/v1/ContactController.java
@@ -1,14 +1,17 @@
 package depavlo.millionredroses.ui.rest.v1;
 
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import depavlo.millionredroses.service.ContactService;
@@ -26,11 +29,39 @@ public class ContactController {
 
 	@GetMapping(value = "", produces = { "application/json" })
 	public ResponseEntity<StreamingResponseBody> listAllContacts(
-			@RequestParam(value = "nameFilter", required = true) String filter, HttpServletResponse response) {
+			@RequestParam(value = "nameFilter", required = true) String filter) {
+
+		log.debug("listAllContacts] - Get Contacts by use filter '{}'", filter);
 
 		StreamingResponseBody body = contactService::writeTo;
 		contactService.setFilter(filter);
 
 		return new ResponseEntity<>(body, HttpStatus.OK);
 	}
+
+	/**
+	 * Handle other exceptions.
+	 *
+	 * @param ex      the Exception
+	 * @param request the WebRequest
+	 * @return the response entity
+	 */
+	@ExceptionHandler(value = { Exception.class })
+	@ResponseBody()
+	public ResponseEntity<Object> handleOtherExceptions(Exception ex, WebRequest request) {
+
+		log.debug("handleOtherExceptions] - Gets exception: {}", ex.getMessage());
+
+		String headers = request.getHeader(HttpHeaders.ACCEPT);
+
+		MediaType mt;
+		if (headers.indexOf(MediaType.APPLICATION_XML_VALUE) == -1) {
+			mt = MediaType.APPLICATION_JSON;
+		} else {
+			mt = MediaType.APPLICATION_XML;
+		}
+
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).contentType(mt).body(ex.getMessage());
+	}
+
 }
